d904415887c16d17e524aadec2a8fa88eb019128
DanielArribas <109543207+DanielArribas@users.noreply.github.com>
2023-10-23 17:12:57 -0400

    Initial Commit

---
+++ Random Pass/Random_PassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_PassApp.swift
+//  Random Pass
+//
+//  Created by Daniel Arribas on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Random_PassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
